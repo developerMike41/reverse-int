11b4f2f0fd829f3d00a5e8a8f879ba8b57cb3962
developerMike41 <hello1mmike41@gmail.com>
2021-02-18 19:17:45 +0300

    feat: task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 0;
+    let last_digit = 0;
+    if (n > 0) {
+        while (n > 0) {
+            last_digit = n % 10;
+            n = Math.floor(n / 10);
+            result *= 10;
+            result += last_digit;
+        }
+    } else if (n < 0) {
+        n *= -1
+        while (n > 0) {
+            last_digit = n % 10;
+            n = Math.floor(n / 10);
+            result *= 10;
+            result += last_digit;
+        }
+    }
+
+    return result;
+};
